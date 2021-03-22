94a8bd6f079a8c15f41b60fa39aeeafdc62e3001
ANDRYXA553 <andriysivik5@gmail.com>
2021-03-22 21:28:49 +0200

    hw4

---
+++ hw4/script.js
@@ -0,0 +1,15 @@
+let  hi ='hello';
+let  name ='owu';
+let   site='com1';
+let  type = 'ua';
+let  nb1 = 1;
+let  nb2 = 10;
+let  nb3 = -999;
+let  nb123 = 123;
+let  nbpi = 3.14;
+let  nb4 = 2.7;
+let  nb5 = 16;
+let valuef=false;
+let valuet=true;
+console.log(hi,name,site,type,nb1,nb2,nb3,nb123,nbpi,nb4,nb5,valuef,valuet)
+
