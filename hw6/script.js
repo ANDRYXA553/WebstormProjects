513d402f6dd591b1207638e3a413e73fcf82daf9
ANDRYXA553 <andriysivik5@gmail.com>
2021-03-30 12:27:13 +0300

    .

---
+++ hw6/script.js
@@ -0,0 +1,209 @@
+// 1) Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+// a) отримує текст з параграфа з id "content"
+
+// let elmentContent = document.getElementById('content');
+// console.log(elmentContent);
+
+// b) отримує текст з блоку з id "rules"
+
+// let elmentRules = document.getElementById('rules');
+// console.log(elmentRules);
+
+// c) замініть текст параграфа з id 'content' на будь-який інший
+//
+// let elmentContent = document.getElementById('content');
+// elmentContent.innerText=''
+//  elmentContent.append('ewqweqweqweqewq')
+// console.log(elmentContent)
+
+// d) замініть текст параграфа з id 'rules' на будь-який інший
+
+// let elmentRules = document.getElementById('rules');
+// elmentRules.innerText=''
+//  elmentRules.append('ewqweqweqweqewq')
+// console.log(elmentRules)
+
+// e) змініть кожному елементу колір фону на червоний
+
+
+
+
+
+// f) змініть кожному елементу колір тексту на синій
+
+// g) отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+
+// let elmentRules = document.getElementById('rules');
+//
+// console.log(elmentRules.classList)
+
+
+// h) отримати всі елементи з класом fc_rules;
+
+//  let fcRulesClass=document.getElementsByClassName('fc_rules')
+//
+// console.log(fcRulesClass);
+
+
+// i) поміняти колір тексту у всіх елементів fc_rules на червоний
+//  let fcRulesClass=document.getElementsByClassName('fc_rules')
+//
+// for (let i = 0; i < fcRulesClass.length; i++) {
+//     fcRulesClass[i].style.backgroundColor='red'
+// }
+
+// 2) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+// Вставити цей блок на сторінку
+
+// let users = [{
+//     name: 'vasya',
+//     age: 31,
+//     status: false,
+//     address: { city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1 }
+// }, {
+//     name: 'petya',
+//     age: 30,
+//     status: true,
+//     address: { city: 'New York', country: 'USA', street: 'East str', houseNumber: 21 }
+// }, {
+//     name: 'kolya',
+//     age: 29,
+//     status: true,
+//     address: { city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78 }
+// }, {
+//     name: 'olya',
+//     age: 28,
+//     status: false,
+//     address: { city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56 }
+// }, {
+//     name: 'max',
+//     age: 30,
+//     status: true,
+//     address: { city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39 }
+// }, {
+//     name: 'anya',
+//     age: 31,
+//     status: false,
+//     address: { city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5 }
+// }, {
+//     name: 'oleg',
+//     age: 28,
+//     status: false,
+//     address: { city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90 }
+// }, {
+//     name: 'andrey',
+//     age: 29,
+//     status: true,
+//     address: { city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33 }
+// }, {
+//     name: 'masha',
+//     age: 30,
+//     status: true,
+//     address: { city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1 }
+// }, {
+//     name: 'olya',
+//     age: 31,
+//     status: false,
+//     address: { city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4 }
+// }, {
+//     name: 'max',
+//     age: 31,
+//     status: true,
+//     address: { city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45 }
+// }];
+//
+// for (let i = 0; i < users.length; i++) {
+//   let divEl = document.createElement('div')
+//     divEl.innerHTML= `<h2>Name:${users[i].name} </h2> <br>
+//        Age: ${users[i].age}<br>
+//        Status:${users[i].status}<br>
+//        adress:${users[i].address.city} , ${users[i].address.country} ,${users[i].address.houseNumber}, ${users[i].address.street}`
+//     document.body.appendChild(divEl);
+// }
+
+
+// 3) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
+// Блок з адресою зробити окремим блоком, з блоками для кожної властивості
+
+// let users = [{
+//     name: 'vasya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+// }, {
+//     name: 'petya',
+//     age: 30,
+//     status: true,
+//     address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+// }, {
+//     name: 'kolya',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+// }, {
+//     name: 'olya',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+// }, {
+//     name: 'max',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+// }, {
+//     name: 'anya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+// }, {
+//     name: 'oleg',
+//     age: 28,
+//     status: false,
+//     address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+// }, {
+//     name: 'andrey',
+//     age: 29,
+//     status: true,
+//     address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+// }, {
+//     name: 'masha',
+//     age: 30,
+//     status: true,
+//     address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+// }, {
+//     name: 'olya',
+//     age: 31,
+//     status: false,
+//     address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+// }, {
+//     name: 'max',
+//     age: 31,
+//     status: true,
+//     address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+// }];
+//
+//
+// for (let i = 0; i < users.length; i++) {
+//     let divEl = document.createElement('div')
+//
+//     divEl.innerHTML = `<h2>Name:${users[i].name} </h2> <br>
+//        Age: ${users[i].age}<br>
+//        Status:${users[i].status}<br>
+//       `
+//
+//     let divAdress = document.createElement('div')
+//     divAdress.style.backgroundColor= 'grey'
+//     divAdress.style.height= '100px'
+//
+//     divAdress.innerHTML = `
+//     adress:${users[i].address.city},
+//     <div>${users[i].address.country} ,</div>
+//     <div>${users[i].address.houseNumber},</div>
+//     <div> ${users[i].address.street}</div> `
+//     document.body.appendChild(divEl);
+//     document.body.appendChild(divAdress);
+//     for (let j = 0; j < users.length; j++) {
+//
+//     }
+// }
