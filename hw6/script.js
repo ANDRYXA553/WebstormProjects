0514d7ef8cc3b0bcb0d2417da177f2df7df39438
ANDRYXA553 <andriysivik5@gmail.com>
2021-03-31 12:22:32 +0300

    hw6 update

--- hw6/script.js
+++ hw6/script.js
@@ -26,9 +26,6 @@
 // e) змініть кожному елементу колір фону на червоний
 
 
-
-
-
 // f) змініть кожному елементу колір тексту на синій
 
 // g) отримати весь список класів елемента з id=rules і вивести їх в console.log
@@ -130,63 +127,80 @@ let users = [{
     name: 'vasya',
     age: 31,
     status: false,
-    address: { city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1 }
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
 }, {
     name: 'petya',
     age: 30,
     status: true,
-    address: { city: 'New York', country: 'USA', street: 'East str', houseNumber: 21 }
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
 }, {
     name: 'kolya',
     age: 29,
     status: true,
-    address: { city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78 }
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
 }, {
     name: 'olya',
     age: 28,
     status: false,
-    address: { city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56 }
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
 }, {
     name: 'max',
     age: 30,
     status: true,
-    address: { city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39 }
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
 }, {
     name: 'anya',
     age: 31,
     status: false,
-    address: { city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5 }
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
 }, {
     name: 'oleg',
     age: 28,
     status: false,
-    address: { city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90 }
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
 }, {
     name: 'andrey',
     age: 29,
     status: true,
-    address: { city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33 }
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
 }, {
     name: 'masha',
     age: 30,
     status: true,
-    address: { city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1 }
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
 }, {
     name: 'olya',
     age: 31,
     status: false,
-    address: { city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4 }
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
 }, {
     name: 'max',
     age: 31,
     status: true,
-    address: { city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45 }
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
 }];
 
- for (let user of users){
-     let divUser = document.createElement('div')
+for (let user of users) {
+    let divUser = document.createElement('div')
+    let divAdress = document.createElement('div')
+    divAdress.style.backgroundColor = 'grey'
+    divAdress.style.margin = '15px 0 15px 0'
 
+    for (let userProp in user) {
+        let divProp = document.createElement('div')
 
+        divProp.innerHTML = `${userProp}:${user[userProp]}`
+        if (userProp === 'address') {
+            divProp.innerHTML = `${userProp}:`
+        }
+        divUser.appendChild(divProp)
+    }
+    for (let adressProp in user.address) {
+        let divAdressProp = document.createElement('div')
+        divAdressProp.innerHTML = `${adressProp}:${user.address[adressProp]}`
+        divAdress.appendChild(divAdressProp)
+        divUser.appendChild(divAdress)
+    }
+    document.body.appendChild(divUser)
 
-
- }
+}
