c37026767d1000144df68832ee092204df43199a
ANDRYXA553 <andriysivik5@gmail.com>
2021-03-31 13:46:58 +0300

    pr6 update

--- pr6/script.js
+++ pr6/script.js
@@ -155,3 +155,40 @@
 //     rulesDiv.innerHTML= `<h2>${rules[i].title}</h2> <p> ${rules[i].body}</p>`
 //     document.body.appendChild(rulesDiv)
 // }
+
+
+//
+// - Дано 2 масиви з рівною кількістю об'єктів.
+// Масиви:
+// let usersWithId = [
+//     {id: 1, name: 'vasya', age: 31, status: false},
+//     {id: 2, name: 'petya', age: 30, status: true},
+//     {id: 3, name: 'kolya', age: 29, status: true},
+//     {id: 4, name: 'olya', age: 28, status: false}
+// ];
+//
+// let citiesWithId = [
+//     {user_id: 3, country: 'USA', city: 'Portland'},
+//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+//     {user_id: 2, country: 'Poland', city: 'Krakow'},
+//     {user_id: 4, country: 'USA', city: 'Miami'}
+// ];
+// //
+// let newArr= [...usersWithId]
+// // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+// //     Записати цей об'єкт в новий масив
+//
+//
+// for (let useridElement of usersWithId) {
+//
+//
+//     for (let citiesIdElement of citiesWithId) {
+//         if (useridElement.id === citiesIdElement.user_id) {
+//
+//             delete citiesIdElement.user_id
+//             newArr.addres = citiesWithId;
+//         }
+//     }
+// }
+// console.log(usersWithId)
+// console.log(newArr);
