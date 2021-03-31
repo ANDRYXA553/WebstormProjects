6be788478cee930a06bae00adc3d4a23c2c89612
ANDRYXA553 <andriysivik5@gmail.com>
2021-03-31 12:09:50 +0300

    hw6 update

--- hw6/script.js
+++ hw6/script.js
@@ -126,84 +126,67 @@
 // 3) За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement.
 // Блок з адресою зробити окремим блоком, з блоками для кожної властивості
 
-// let users = [{
-//     name: 'vasya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
-// }, {
-//     name: 'petya',
-//     age: 30,
-//     status: true,
-//     address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
-// }, {
-//     name: 'kolya',
-//     age: 29,
-//     status: true,
-//     address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
-// }, {
-//     name: 'olya',
-//     age: 28,
-//     status: false,
-//     address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
-// }, {
-//     name: 'max',
-//     age: 30,
-//     status: true,
-//     address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
-// }, {
-//     name: 'anya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
-// }, {
-//     name: 'oleg',
-//     age: 28,
-//     status: false,
-//     address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
-// }, {
-//     name: 'andrey',
-//     age: 29,
-//     status: true,
-//     address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
-// }, {
-//     name: 'masha',
-//     age: 30,
-//     status: true,
-//     address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
-// }, {
-//     name: 'olya',
-//     age: 31,
-//     status: false,
-//     address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
-// }, {
-//     name: 'max',
-//     age: 31,
-//     status: true,
-//     address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
-// }];
-//
-//
-// for (let i = 0; i < users.length; i++) {
-//     let divEl = document.createElement('div')
-//
-//     divEl.innerHTML = `<h2>Name:${users[i].name} </h2> <br>
-//        Age: ${users[i].age}<br>
-//        Status:${users[i].status}<br>
-//       `
-//
-//     let divAdress = document.createElement('div')
-//     divAdress.style.backgroundColor= 'grey'
-//     divAdress.style.height= '100px'
-//
-//     divAdress.innerHTML = `
-//     adress:${users[i].address.city},
-//     <div>${users[i].address.country} ,</div>
-//     <div>${users[i].address.houseNumber},</div>
-//     <div> ${users[i].address.street}</div> `
-//     document.body.appendChild(divEl);
-//     document.body.appendChild(divAdress);
-//     for (let j = 0; j < users.length; j++) {
-//
-//     }
-// }
+let users = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: { city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1 }
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: { city: 'New York', country: 'USA', street: 'East str', houseNumber: 21 }
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: { city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78 }
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: { city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56 }
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: { city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39 }
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: { city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5 }
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: { city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90 }
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: { city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33 }
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: { city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1 }
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: { city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4 }
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: { city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45 }
+}];
+
+ for (let user of users){
+     let divUser = document.createElement('div')
+
+
+
+
+ }
