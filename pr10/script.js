4c1e7e8c955e98bb9197ba3ff47dca5856737f82
ANDRYXA553 <andriysivik5@gmail.com>
2021-04-13 20:06:38 +0300

    pr10

---
+++ pr10/script.js
@@ -0,0 +1,55 @@
+// - Сворити масив нецензцрних слів.
+//     Сворити інпут текстового типу.
+//     Якщо людина вводить слово і воно міститься в масиві нецензурних слів кинути алерт з попередженням.
+//     Перевірку робити при натисканні на кнопку
+
+// let xWords = ['qwert', 'ewq', 'xxx']
+//
+// let inputText = document.getElementById('inputText')
+// let button = document.getElementById('buttonCheck')
+//
+//
+// button.onclick = () => {
+//     for (const xWord of xWords) {
+//         if (inputText.value.toLowerCase()==xWord){
+//             alert('НЕГАРНО ТАК ПИСАТИ')
+//         }
+//
+//     }
+// }
+
+// - Сворити масив нецензцрних слів.
+//     Сворити інпут текстового типу.
+//     Потрібно перевіряти чи не містить ціле речення в собі погані слова.
+//     Кинути алерт з попередженням у випадку якщо містить.
+//     Перевірку робити при натисканні на кнопку
+//
+// let xWords = ['qwert', 'ewq', 'xxx']
+//
+// let inputText = document.getElementById('inputText')
+// let button = document.getElementById('buttonCheck')
+//
+//
+//
+// button.onclick = () => {
+//     for (const xWord of xWords) {
+//         if (inputText.value.toLowerCase().indexOf(xWord)!==-1){
+//             alert('НЕГАРНО ТАК ПИСАТИ')
+//         }
+//
+//     }
+// }
+//
+// -- Створити скрипт, котрий бере зчитує на сторінці (rules.html) текст і робить збоку меню-зміст по всіх заголовках які є в тексті.
+//     При кліку на пункт заголовку ви маєте відправлятись  до цього пункту в тексті
+
+let hs = document.getElementsByTagName('h2')
+
+for (let i = 0; i < hs.length; i++) {
+    let menudiv = document.createElement('div ')
+hs[i].ad
+    menudiv.innerHTML = `<p><a href=`${hs[i]}`> </a></p>`
+
+
+`
+}
