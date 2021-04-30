d6ec679b310e7fcfd544de56e0b0ff5f9daeae30
ANDRYXA553 <andriysivik5@gmail.com>
2021-04-30 18:02:19 +0300

    hw2

--- reactHw/hw2/src/App.js
+++ reactHw/hw2/src/App.js
@@ -1,53 +1,74 @@
-import React,{useState} from 'react'
+import React, {useState} from 'react'
 
 import './App.css';
 import User from "./components/userComponent/userComponent";
 
 
 function App() {
-    let usersList = [
-        {name: 'vasya', age: 31, status: false},
-        {name: 'petya', age: 30, status: true},
-        {name: 'kolya', age: 29, status: true},
-        {name: 'olya', age: 28, status: false},
-        {name: 'max', age: 30, status: true},
-        {name: 'anya', age: 31, status: false},
-        {name: 'oleg', age: 28, status: false},
-        {name: 'andrey', age: 29, status: true},
-        {name: 'masha', age: 30, status: true},
-        {name: 'olya', age: 31, status: false},
-        {name: 'max', age: 31, status: true}
-    ];
+    // let usersList = [
+    //     {name: 'vasya', age: 31, status: false},
+    //     {name: 'petya', age: 30, status: true},
+    //     {name: 'kolya', age: 29, status: true},
+    //     {name: 'olya', age: 28, status: false},
+    //     {name: 'max', age: 30, status: true},
+    //     {name: 'anya', age: 31, status: false},
+    //     {name: 'oleg', age: 28, status: false},
+    //     {name: 'andrey', age: 29, status: true},
+    //     {name: 'masha', age: 30, status: true},
+    //     {name: 'olya', age: 31, status: false},
+    //     {name: 'max', age: 31, status: true}
+    // ];
+    //  usersList.map((value, index) => usersList[index].id = index)
 
-    let [users,setUsers]=useState(usersList);
-
-    const deleteUser=()=>{
-        users.pop()
-        setUsers([...users])
-    }
-
-let [counter,doSomething]=useState(0);
-    const incrementCounter=()=>{
+    let [counter, doSomething] = useState(0);
+    const incrementCounter = () => {
         ++counter
-    doSomething(counter)
+        doSomething(counter)
     }
-    const decrementCounter=()=>{
+    const decrementCounter = () => {
         --counter
-    doSomething(counter)
+        doSomething(counter)
     }
-    const reset=()=>{
-        counter=0
-    doSomething(counter)
+    const reset = () => {
+        counter = 0
+        doSomething(counter)
+    }
+
+    let [usersToDelete, deletedUsers] = useState([
+        {name: 'vasya', age: 31, status: false, id: 0},
+        {name: 'petya', age: 30, status: true, id: 1},
+        {name: 'kolya', age: 29, status: true, id: 2},
+        {name: 'olya', age: 28, status: false, id: 3},
+        {name: 'max', age: 30, status: true, id: 4},
+        {name: 'anya', age: 31, status: false, id: 5},
+        {name: 'oleg', age: 28, status: false, id: 6},
+        {name: 'andrey', age: 29, status: true, id: 7},
+        {name: 'masha', age: 30, status: true, id: 8},
+        {name: 'olya', age: 31, status: false, id: 9},
+        {name: 'max', age: 31, status: true, id: 10}
+    ])
+    console.log(usersToDelete)
+
+    const deleteUS = (value) => {
+        console.log(value)
+        const newArr = [...usersToDelete]
+        if (newArr[value].id === usersToDelete[value].id) {
+            newArr.splice(value, 1)
+            deletedUsers(newArr)
+            console.log(usersToDelete)
+        }
+
     }
 
     return (
         <div className="App">
 
-            {users.map((value, index)=>
-                <User key={index}
-                    {...value}/>
+            {usersToDelete.map((value, index) => {
+
+                    return <User key={index}
+                                 {...value} index={index} func={deleteUS}/>
+                }
             )}
-            <button onClick={deleteUser}>Delete</button>
 
 
             <div>{counter}</div>
@@ -59,3 +80,4 @@ let [counter,doSomething]=useState(0);
 }
 
 export default App;
+
--- reactHw/hw2/src/components/userComponent/userComponent.js
+++ reactHw/hw2/src/components/userComponent/userComponent.js
@@ -1,14 +1,19 @@
 import React from 'react'
- function User(props){
-    let {name,age,status}=props;
-  return (
-      <div>
-          {name}
-          {age}
-          {status}
-      </div>
-  )
+
+function User(props) {
+    let {name, age, status,func,index} = props;
+
+
+    return (
+        <div >
+            {name}
+            {age}
+            {status}
+            <button onClick={()=>func(index)}>Delet11e</button>
+        </div>
+
+    )
 
 };
- export default User;
+export default User;
 
