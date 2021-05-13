e97560022ccd6b9ae4675cf297ddc284c9806654
ANDRYXA553 <andriysivik5@gmail.com>
2021-05-13 10:26:37 +0300

    pr react 1

--- reactHw/hw4/src/App.js
+++ reactHw/hw4/src/App.js
@@ -1,57 +1,25 @@
-import React, {useEffect, useState} from 'react'
+import logo from './logo.svg';
 import './App.css';
-import Characters from "./components/characters/characters";
-import Character from "./components/characters/character/character";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useRouteMatch,
-    useParams
-} from "react-router-dom";
-import Inventory from "./components/inventory/inventory";
-import InventoryItem from "./components/inventory/inventoryItem/inventoryItem";
-
 
 function App() {
-    let [characters1, setCharacters1] = useState([]);
-
-    useEffect(() => {
-        fetch('https://api.sampleapis.com/futurama/inventory').then(value => value.json().then(value => {
-                let filteredValue1 = value.filter((item, index) => index < 9
-                )
-
-                setCharacters1(filteredValue1)
-            })
-        )
-
-    }, [])
-    return (
-        <Router>
-            <div className="App">
-                < Link to={'/posts'}>Posts</Link>-
-                < Link to={'/inventory'}>Inventory</Link>
-                <Switch>
-                    <Route path={'/posts'} exact={true} render={(value) => {
-
-                        return <Characters/>
-                    }}/>
-                    <Route path={'/posts/:id'} render={(props => {
-                        return <Character/>
-                    })}>
-                    </Route>
-                    <Route path={'/inventory'} exact={true} render={(value) => {
-
-                        return <Inventory/>
-                    }}/>
-                    <Route path={'/inventory/:id'}>
-                      <InventoryItem obj={characters1}/>
-
-                    </Route>
-                </Switch>
-            </div>
-        </Router>)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
