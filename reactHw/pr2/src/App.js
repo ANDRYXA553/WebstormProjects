6ea588e1f0689be868140bead63ab52d38057d2e
ANDRYXA553 <andriysivik5@gmail.com>
2021-05-13 19:27:46 +0300

    pr2

--- reactHw/pr2/src/App.js
+++ reactHw/pr2/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Details from "./components/details/details";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [pageNum, setPageNum] = useState(1)
+
+    let [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch(`https://reqres.in/api/users?page=${pageNum}`).then(value => value.json()).then(value => {
+
+            setUsers(value.data)
+        })
+    }, [pageNum])
+
+
+    return (<Router>
+            <div className="App">
+                <Link to={'/users'}>Users </Link>
+                <Route exact path={'/users'}>
+                </Route>
+                {users.map(item => <>
+                        <Link to={`/users/${item.id}`}>
+                            <div>{item.id}-{item.first_name}
+                            </div>
+                        </Link>
+                        <Route path={`/users/${item.id}`}><Details item={item}/></Route>
+                    </>
+                )}
+                <button onClick={() => {
+                    (pageNum < 3) ? setPageNum(pageNum += 1) : setPageNum(pageNum = 1)
+
+                }}>NEXT
+                </button>
+                <button onClick={() => {
+                    (pageNum > 1) ? setPageNum(pageNum -= 1) : setPageNum(pageNum = 3)
+                }}>PREVIOUS
+                </button>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ reactHw/pr2/src/components/details/details.js
@@ -0,0 +1,5 @@
+export default function Details(item) {
+
+    return <div>{item.item.id}-{item.item.email}</div>
+
+}
