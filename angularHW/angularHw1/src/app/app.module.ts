84ec8f5d580088103b6bee2164a39f04e6b374a7
ANDRYXA553 <andriysivik5@gmail.com>
2021-05-27 11:07:42 +0300

    angularHw1

--- angularHW/angularHw1/src/app/app.module.ts
+++ angularHW/angularHw1/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users/users.component';
+import { PostsComponent } from './components/posts/posts/posts.component';
+import { UserComponent } from './components/users/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PostsComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule
